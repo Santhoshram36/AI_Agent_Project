1169aca265d68e4d1b95c8c632872a3fbec2f272
Santhoshram36 <112062894+Santhoshram36@users.noreply.github.com>
2025-03-16 18:36:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from langchain.schema import HumanMessage
-from langgraph_agent_example import agent
+from langgraph_agent import agent
 
 app = FastAPI()
 
